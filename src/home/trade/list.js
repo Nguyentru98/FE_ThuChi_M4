9395e4a57cf88f9d18ef37d7ed8d358ed94cd89a
Tru Nguyen <nguyentruhn@gmail.com>
2023-08-07 09:41:55 +0700

    form ADD EDIT

--- src/home/trade/list.js
+++ src/home/trade/list.js
@@ -19,107 +19,106 @@ function listTrade() {
                         
                     </tr>`
         })
-        document.getElementById('display').innerHTML = str;
+        document.getElementById('list').innerHTML = str;
     })
 }
 
-//     function save() {
-//     let data = {
-//     id: document.getElementById('id').value,
-//     name: document.getElementById('name').value,
-//     amount: document.getElementById('amount').value,
-//     date: document.getElementById('date').value,
-//     type: document.getElementById('type').value,
-//     currentMoney: document.getElementById('currentMoney').value,
-// };
+    function save() {
+    let data = {
+    id: document.getElementById('id').value,
+    name: document.getElementById('name').value,
+    amount: document.getElementById('amount').value,
+    date: document.getElementById('date').value,
+    type: document.getElementById('type').value,
+    currentMoney: document.getElementById('currentMoney').value,
+};
 
 
-//     axios.post(API_URL, data).then((res) => {
-//     load();
-// });
-// }
+    axios.post(API_URL, data).then((res) => {
+    load();
+});
+}
 
-//     function showFromAdd() {
-//     let str = `
-//       <div class="mx-auto card p-4 my-4 w-75 shadow-lg rounded">
-//          <h2 class="text-center">Add Transaction</h2>
-//          <form>
-//             <div class="form-group">
-//                <label for="">ID</label>
-//                <input type="text" class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter ID">
-//             </div>
-//             <div class="form-group">
-//                <label for="">Name</label>
-//                <input type="text" class="form-control" id="name" placeholder="Name">
-//             </div>
-//             <div class="form-group">
-//                <label for="">Amount</label>
-//                <input type="text" class="form-control" id="amount" placeholder="Amount">
-//             </div>
-//             <div class="form-group">
-//                <label for="">Date</label>
-//                <input type="date" class="form-control" id="date" placeholder="Date">
-//             </div>
-//             <div class="form-group">
-//                <label for="">Type</label>
-//                <input type="text" class="form-control" id="type" placeholder="Type">
-//             </div>
-//             <div class="form-group">
-//                <label for="">currentMoney</label>
-//                <input type="text" class="form-control" id="currentMoney" placeholder="currentMoney">
-//             </div>
-//             <div class="d-flex justify-content-start">
-//                <button type="button" onclick="save()" class="btn btn-primary mt-4 mr-2">Xác nhận</button>
-//                <button type="button" class="btn btn-secondary mt-4">Thoát</button>
-//             </div>
-//          </form>
-//       </div>`;
-//     document.getElementById('display').innerHTML = str;
-// }
+    function showFromAdd() {
+    let str = `
+      <div class="mx-auto card p-4 my-4 w-75 shadow-lg rounded">
+         <h2 class="text-center">Add Edit Transaction</h2>
+         <form>
+            <div class="form-group">
+               <label for="">ID</label>
+               <input type="text" class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter ID">
+            </div>
+            <div class="form-group">
+               <label for="">Name</label>
+               <input type="text" class="form-control" id="name" placeholder="Name">
+            </div>
+            <div class="form-group">
+               <label for="">Amount</label>
+               <input type="text" class="form-control" id="amount" placeholder="Amount">
+            </div>
+            <div class="form-group">
+               <label for="">Date</label>
+               <input type="date" class="form-control" id="date" placeholder="Date">
+            </div>
+            <div class="form-group">
+               <label for="">Type</label>
+               <input type="text" class="form-control" id="type" placeholder="Type">
+            </div>
+            <div class="form-group">
+               <label for="">currentMoney</label>
+               <input type="text" class="form-control" id="currentMoney" placeholder="currentMoney">
+            </div>
+            <div class="d-flex justify-content-start">
+               <button type="button" onclick="save()" class="btn btn-primary mt-4 mr-2">Save</button> 
+            </div>
+         </form>
+      </div>`;
+    document.getElementById('formAdd').innerHTML = str;
+}
 
-//     function deleteTrade(id) {
-//     axios.delete(API_URL + `/${id}`).then((res) => {
-//         load();
-//     })
-// }
+function deleteTrade(id) {
+    axios.delete(API_URL + `/${id}`).then((res) => {
+        load();
+    })
+}
 
-//     function showFromEdit(id) {
-//     axios.get(API_URL + `/${id}`).then(res => {
-//         let data = res.data[0];
+    function showFromEdit(id) {
+    axios.get(API_URL + `/${id}`).then(res => {
+        let data = res.data[0];
 
-//         let str = ``
+        let str = ``
 
-//         document.getElementById('display').innerHTML = str;
-
-
-//         document.getElementById('id').value = data.id;
-//         document.getElementById("name").value = data.name;
-//         document.getElementById("amount").value = data.amount;
-//         document.getElementById("date").value = data.date;
-//         document.getElementById("type").value = data.type;
-//         document.getElementById("currentMoney").value = data.currentMoney;
-//     })
-// }
+        document.getElementById('display').innerHTML = str;
 
 
-
-//     function update() {
-//     let data = {
-//     id: document.getElementById('id').value,
-//     name: document.getElementById('name').value,
-//     amount: document.getElementById('amount').value,
-//     date: document.getElementById('date').value,
-//     type: document.getElementById('type').value,
-//     // currentMoney: document.getElementById('currentMoney').value,
-
-// }
+        document.getElementById('id').value = data.id;
+        document.getElementById("name").value = data.name;
+        document.getElementById("amount").value = data.amount;
+        document.getElementById("date").value = data.date;
+        document.getElementById("type").value = data.type;
+        document.getElementById("currentMoney").value = data.currentMoney;
+    })
+}
 
 
 
-//     axios.put(`${API_URL}/${data.id}`, data).then((res) => {
-//     axios.get(API_URL).then((res) => {
-//     load();
-// })
-// })
-// }
+    function update() {
+    let data = {
+    id: document.getElementById('id').value,
+    name: document.getElementById('name').value,
+    amount: document.getElementById('amount').value,
+    date: document.getElementById('date').value,
+    type: document.getElementById('type').value,
+    // currentMoney: document.getElementById('currentMoney').value,
+
+}
+
+
+
+    axios.put(`${API_URL}/${data.id}`, data).then((res) => {
+    axios.get(API_URL).then((res) => {
+    load();
+})
+})
+}
 
