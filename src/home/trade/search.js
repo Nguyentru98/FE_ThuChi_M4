6a33efcc8825aaba288be3eec5e157ff77fe746b
Tru Nguyen <nguyentruhn@gmail.com>
2023-08-07 23:19:01 +0700

    xong search

--- src/home/trade/search.js
+++ src/home/trade/search.js
@@ -1,73 +1,27 @@
 function search() {
-    let name = document.getElementById('Trading name').value ;
+    let name = document.getElementById('name').value ;
     let date = document.getElementById('date').value ;
     let type = document.getElementById('type').value ;
+
+    axios.get(`http://localhost:3000/trades?date=${date}&type=${type}&name=${name}`).then((res) => {
+        let traders = res.data;
+        console.log(traders)
+        let str ='';
+        traders.map(item => {
+            str += `
+            <tr>
+                        <td><input class="form-check-input" type="checkbox"></td>
+                        <td>${item.date}</td>
+                        <td>${item.name}</td>
+                        <td>${item.type}</td>
+                        <td>${item.amount}</td>
+                        <td> <a class="btn btn-sm btn-primary" href="${item.id}">EDIT</a>
+                                <a class="btn btn-sm btn-primary" href="${item.id}">DELETE</a>
+                        </td>
+                        
+                    </tr>`
+        })
+        document.getElementById('list').innerHTML = str;
+    })
     
-    if(name ='') {
-        axios.get(`http://localhost:3000/trades/date=${date}&type=${type}`).then((res) => {
-            let data = res.data;
-            console.log(data)
-            let str ='';
-            data.map(item => {
-                str += `
-                <tr>
-                            <td><input class="form-check-input" type="checkbox"></td>
-                            <td>${item.date}</td>
-                            <td>${item.name}</td>
-                            <td>${item.type}</td>
-                            <td>${item.amount}</td>
-                            <td> <a class="btn btn-sm btn-primary" href="${item.id}">EDIT</a>
-                                 <a class="btn btn-sm btn-primary" href="${item.id}">DELETE</a>
-                            </td>
-                            
-                        </tr>`
-            })
-            document.getElementById('list').innerHTML = str;
-        })
-    }
-    else if(date ='') {
-        axios.get(`http://localhost:3000/trades/?name=${name}&type=${type}`).then((res) => {
-        let data = res.data;
-        console.log(data)
-        let str ='';
-        data.map(item => {
-            str += `
-            <tr>
-                        <td><input class="form-check-input" type="checkbox"></td>
-                        <td>${item.date}</td>
-                        <td>${item.name}</td>
-                        <td>${item.type}</td>
-                        <td>${item.amount}</td>
-                        <td>${item.userId}</td>
-                        <td> <a class="btn btn-sm btn-primary" href="${item.id}">EDIT</a>
-                             <a class="btn btn-sm btn-primary" href="${item.id}">DELETE</a>
-                        </td>
-                        
-                    </tr>`
-        })
-        document.getElementById('list').innerHTML = str;
-    })
-    }else {
-        axios.get(`http://localhost:3000/trades/?name=${name}&date=${date}&type=${type}`).then((res) => {
-        let data = res.data;
-        console.log(data)
-        let str ='';
-        data.map(item => {
-            str += `
-            <tr>
-                        <td><input class="form-check-input" type="checkbox"></td>
-                        <td>${item.date}</td>
-                        <td>${item.name}</td>
-                        <td>${item.type}</td>
-                        <td>${item.amount}</td>
-                        <td>${item.userId}</td>
-                        <td> <a class="btn btn-sm btn-primary" href="${item.id}">EDIT</a>
-                             <a class="btn btn-sm btn-primary" href="${item.id}">DELETE</a>
-                        </td>
-                        
-                    </tr>`
-        })
-        document.getElementById('list').innerHTML = str;
-    })
-    }
 }
--- src/home/trade/totalMoney.js
+++ src/home/trade/totalMoney.js
@@ -1,5 +1,5 @@
 function totalPay() {
-    axios.get('http://localhost:3000/trades/?type=Pay').then((res) => {
+    axios.get('http://localhost:3000/trades/?type=2').then((res) => {
         let data = res.data
         let sumPay = 0;
         for (let i = 0; i < data.length; i++) {
@@ -10,17 +10,13 @@ function totalPay() {
         let str = `
         <p class="mb-2">total Pay</p>
         <h6 class="mb-0">$ ${sumPay}</h6>`
-
-        console.log("totalPay")
-        console.log(sumPay);
-        console.log(str)
         document.getElementById('chi').innerHTML = str
 
     })
 }
 
 function totalRevenue() {
-    axios.get('http://localhost:3000/trades/?type=Revenue').then((res) => {
+    axios.get('http://localhost:3000/trades/?type=1').then((res) => {
         let data = res.data
         let sumRevenue = 0;
         for (let i = 0; i < data.length; i++) {
@@ -29,40 +25,10 @@ function totalRevenue() {
         let str = `
         <p class="mb-2">total Revenue</p>
         <h6 class="mb-0">$ ${sumRevenue}</h6>`
-
-        console.log("totalRevenue")
-        console.log(sumRevenue);
-        console.log(str)
         document.getElementById('thu').innerHTML = str
 
     })
 }
-// function totalMoney() {
-//     let sumRevenue = 0;
-//     let sumPay = 0;
-//     let totalMoney = 0;
-    
-//     const revenueResponse = axios.get('http://localhost:3000/trades/?type=thu').then((res) => {
-//             let data = revenueResponse.data
-//             for (let i = 0; i < data.length; i++) {
-//                 sumRevenue+= data[i].amount 
-//         }
-//     })
-//     const payResponse = axios.get('http://localhost:3000/trades/?type=chi').then((res) => {
-//             let data = payResponse.data
-//             for (let i = 0; i < data.length; i++) {
-//                 sumPay+= data[i].amount 
-//         }
-//     })
-//     totalMoney = sumRevenue - sumPay
-//     let str =`<p class="mb-2">Today Money</p>
-//             <h6 class="mb-0">$ ${totalMoney}</h6>`
-
-//     console.log("totalMoney")
-//     console.log(totalMoney)
-
-//     document.getElementById('totalMoney').innerHTML = str
-// }
 
 async function totalMoney() {
     let sumRevenue = 0;
@@ -71,13 +37,13 @@ async function totalMoney() {
     
   
     try {
-      const revenueResponse = await axios.get('http://localhost:3000/trades/?type=Revenue');
+      const revenueResponse = await axios.get('http://localhost:3000/trades/?type=1');
       const revenueData = revenueResponse.data;
       for (let i = 0; i < revenueData.length; i++) {
         sumRevenue += revenueData[i].amount;
       }
   
-      const payResponse = await axios.get('http://localhost:3000/trades/?type=Pay');
+      const payResponse = await axios.get('http://localhost:3000/trades/?type=2');
       const payData = payResponse.data;
       for (let i = 0; i < payData.length; i++) {
         sumPay += payData[i].amount;
