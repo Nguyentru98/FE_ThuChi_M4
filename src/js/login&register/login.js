d527e3b19cfae1fcc5f17521905e5a3cd8ba0a61
Nghia Bui <nghia@MacBook-Air.local>
2023-08-08 08:20:12 +0700

    login

--- src/js/login&register/login.js
+++ src/js/login&register/login.js
@@ -1,14 +1,5 @@
 function loadLogin(){
     document.getElementById('display').innerHTML = `<div class="container-xxl position-relative bg-white d-flex p-0">
-        <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
-        <!-- Spinner End -->
-
-
         <!-- Sign In Start -->
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
@@ -36,7 +27,7 @@ function loadLogin(){
                             <a href="">Forgot Password</a>
                         </div>
                         <button onclick="checkUser()" type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                        <p class="text-center mb-0">Don't have an Account? <a onclick="registerForm()">Sign Up</a></p>
+                        <p class="text-center mb-0">Don't have an Account? <a class="btn btn-primary py-3 w-100 mb-4" onclick="registerForm()">Sign Up</a></p>
                     </div>
                 </div>
             </div>
@@ -56,23 +47,11 @@ function checkUser(){
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
         // Xử lý dữ liệu trả về từ server nếu cần
         alert('Đăng nhập thành công!');
-        loadTrades();
+        loadHome();
     }).catch(error => {
         console.error(error);
         alert('Đăng nhập không thành công. Vui lòng kiểm tra tên đăng nhập và mật khẩu và thử lại sau.');
     });
-}
-function loadTrades(){
-    axios.get('http://localhost:3000/trades').then((res) =>{
-        let data =res.data
-        let str = ''
-        data.map(item => {
-            str=`<h1>Danh sach</h1>
-${item.id}: ${item.name} 
-             `
-        })
-        document.getElementById('display').innerHTML = str
-    })
 }
 function registerUser() {
     let data = {
@@ -117,11 +96,8 @@ function registerForm(){
                             <label for="floatingPassword">Current Money</label>
                         </div>
                         <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button onclick="registerUser()">Sign in</button>
+                            <button class="btn btn-primary py-3 w-100 mb-4" onclick="registerUser()">Sign up</button><br>
+                            <button class="btn btn-primary py-3 w-100 mb-4" onclick="loadLogin()">Sign in</button>
                         </div>
                        
                     </div>
