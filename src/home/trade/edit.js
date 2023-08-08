48ecf90c0c629b4ea4da129d71333027b1e63571
Tru Nguyen <nguyentruhn@gmail.com>
2023-08-08 11:57:03 +0700

    edit dang sua id

--- src/home/trade/edit.js
+++ src/home/trade/edit.js
@@ -13,3 +13,20 @@ function findById(id) {
          document.getElementById('tradeTypeAE').value= data.tradeTypeId
   });
 }
+
+function update(id) {
+   let data = {
+      userId: document.getElementById('userAE').value,
+      name: document.getElementById('nameAE').value,
+      date: document.getElementById('dateAE').value,
+      amount: document.getElementById('amountAE').value,
+      type: document.getElementById('typeAE').value,
+      tradeTypeId: document.getElementById('tradeTypeAE').value,
+  };
+   
+   let a = axios.put('http://localhost:3000/trades/' +id, data).then(res=>{
+      console.log(id)
+       console.log(a)
+       listTrade()
+   })
+}
